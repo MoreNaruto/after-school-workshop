2c59bfdc8a3dbae417f8e8b1b692ae978a089414
tmorris <tmorris@thoughtworks.com>
2015-04-29 10:57:06 -0500

    Added a new events page

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150429150211) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: true do |t|
+    t.string   "name"
+    t.date     "start_date"
+    t.date     "end_date"
+    t.string   "creator"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
