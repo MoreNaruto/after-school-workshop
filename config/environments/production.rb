c85ee258f1883fcce8a756a81980d88ef38c33b3
tmorris <tmorris@thoughtworks.com>
2015-05-11 17:11:45 -0500

    Trying to get pictures to work on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -36,8 +36,8 @@ Rails.application.configure do
   config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
   
   # Precompile additional assets
-  config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
-
+  config.assets.precompile =  ['*.js', '*.css', '*.css.erb', '*.jpg', '*.png'] 
+  
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Specifies the header that your server uses for sending files.
