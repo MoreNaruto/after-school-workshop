d712e10d1d5691e936e73e678207dec580209e5a
tmorris <tmorris@thoughtworks.com>
2015-05-22 09:57:23 -0500

    Fixed test database with new migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150515212303) do
+ActiveRecord::Schema.define(version: 20150522141150) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,12 +23,7 @@ ActiveRecord::Schema.define(version: 20150515212303) do
     t.string   "creator"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
     t.string   "description"
-    t.string   "dropbox_session"
   end
 
 end
