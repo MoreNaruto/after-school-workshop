6779f922e57c884cc25ce251f7007fb988cd1b41
tmorris <tmorris@thoughtworks.com>
2015-05-26 16:56:29 -0500

    Display image on events page

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,9 @@
 class Event < ActiveRecord::Base
     has_attached_file :image,
-    :styles => {:medium => "300x300", :thumb => "200x200"},
+    :styles => {:medium => "300x300", :thumb => "100x100"},
     :storage => :dropbox,
     :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-    :dropbox_options => {:path => proc { |style| "files/#{image.original_filename}_#{id}"} }
+    :dropbox_options => {:path => proc { |style| "images/#{id}_#{image.original_filename}"} },
+    :dropbox_visibility => 'public'
     validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
